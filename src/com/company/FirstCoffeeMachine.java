d996e27d722d402131f15d81e374ba289cffefab
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-26 14:39:27 +0300

    Creation of CoffeeMachine

--- src/com/company/FirstCoffeeMachine.java
+++ src/com/company/FirstCoffeeMachine.java
@@ -13,7 +13,7 @@ public class FirstCoffeeMachine extends CoffeeMachine {
 
     @Override
     public void menu() {
-        System.out.println("Enter your choice: \n1. show \n2. americano \n3. espresso \n4. doubleEspresso");
+        System.out.println("Enter your choice: \n1. show \n2. americano \n3. espresso \n4. doubleEspresso \n5. fill trunk with water");
         String str = sc.next();
         switch (str) {
             case ("1"):
@@ -28,13 +28,16 @@ public class FirstCoffeeMachine extends CoffeeMachine {
                 break;
             case ("4"):
                 doubleEspresso();
+            case ("5"):
+                fillWithWater();
+                case("6")
         }
     }
 
     @Override
     public void show() {
-        System.out.println("The value of coffee in that FirstCoffeeMachine: " + this.getCoffeeTrunk());
-        System.out.println("The value of water in that FirstCoffeeMachine: " + this.getWaterTrunk());
+        System.out.println("The value of coffee in that FirstCoffeeMachine: " + this.getCoffeeTrunk() + "grm");
+        System.out.println("The value of water in that FirstCoffeeMachine: " + this.getWaterTrunk() + "ml");
 
     }
 
@@ -44,11 +47,11 @@ public class FirstCoffeeMachine extends CoffeeMachine {
         String str2 = sc.next();
         if (str2.equals("1")) {
             int americano = (getWaterTrunk() - 2) + (getCoffeeTrunk() - 6) + 3;
-            System.out.println("Your americano is done with amount of liquid : " + americano);
+            System.out.println("Your americano is done with amount of liquid : " + americano+ "ml");
 
         } else if (str2.equals("2")) {
             int americano = (getWaterTrunk() - 2) + (getCoffeeTrunk() - 6);
-            System.out.println("Your americano is done with amount of liquid : " + americano);
+            System.out.println("Your americano is done with amount of liquid : " + americano+ "ml");
 
         }
 
@@ -56,22 +59,59 @@ public class FirstCoffeeMachine extends CoffeeMachine {
 
     @Override
     public void espresso() {
+        System.out.println("Do you want to add sugar:  \n 1) Yes \n 2) No");
+        String str2 = sc.next();
+        if (str2.equals("1")) {
+            int espresso = (getWaterTrunk() - 6) + (getCoffeeTrunk() - 4) + 3;
+            System.out.println("Your americano is done with amount of liquid : " + espresso+ "ml");
 
+        } else if (str2.equals("2")) {
+            int espresso = (getWaterTrunk() - 6) + (getCoffeeTrunk() - 4);
+            System.out.println("Your americano is done with amount of liquid : " + espresso + "ml");
+
+        }
     }
 
     @Override
     public void doubleEspresso() {
+        System.out.println("Do you want to add sugar:  \n 1) Yes \n 2) No");
+        String str2 = sc.next();
+        if (str2.equals("1")) {
+            int doubleEspresso = (getWaterTrunk() - 6) + (getCoffeeTrunk() - 8) + 3;
+            System.out.println("Your americano is done with amount of liquid : " +  doubleEspresso + "ml");
 
+        } else if (str2.equals("2")) {
+            int doubleEspresso = (getWaterTrunk() - 6) + (getCoffeeTrunk() - 8);
+            System.out.println("Your americano is done with amount of liquid : " + doubleEspresso + "ml");
+
+        }
     }
 
     @Override
     public void fillWithWater() {
+       if (getWaterTrunk()==8){
+           System.out.println("trunk is full : " + getWaterTrunk() + "ml");
+       }else if(getWaterTrunk()<8) {
+           System.out.println("Adding the water :");
 
-    }
+           for (int i = 8; i > getWaterTrunk(); i++) {
+               System.out.println("Adding the water :" + i +" ml");
+
+           }
+       }
+        }
 
     @Override
     public void fillWithCoffee() {
+        if (getCoffeeTrunk()==5){
+            System.out.println("trunk is full : " + getCoffeeTrunk() + "grm");
+        }else if(getWaterTrunk()<5) {
+            System.out.println("Adding the coffee :");
 
+            for (int i = 5; i > getCoffeeTrunk(); i++) {
+                System.out.println("Adding the coffee :" + i + " grm");
+            }
+            }
     }
 
     @Override
